85e2f55361e5f7e5abbe83310ab5ace3a8d79efd
Felipe Fernandes <felipedfe@gmail.com>
2022-06-03 16:48:52 -0300

    requisito 2

---
+++ desafio2.sql
@@ -0,0 +1,9 @@
+SELECT
+COUNT(can.cancoes_id) AS cancoes, 
+COUNT(art.artista_id) AS artistas,
+COUNT(alb.album_id) AS albuns
+	FROM SpotifyClone.cancoes AS can
+	LEFT JOIN SpotifyClone.artista AS art
+		ON can.cancoes_id = art.artista_id
+	LEFT JOIN SpotifyClone.album AS alb
+		ON can.cancoes_id = alb.album_id;
