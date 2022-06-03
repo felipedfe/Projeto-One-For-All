97cd245b3b256c836762c199c8908050d5c9800f
Felipe Fernandes <felipedfe@gmail.com>
2022-06-03 17:18:18 -0300

    requisito 3

---
+++ desafio3.sql
@@ -0,0 +1,12 @@
+SELECT
+usu.nome AS usuario, 
+COUNT(hist.usuario_id) AS qtde_musicas_ouvidas,
+ROUND(SUM(can.duracao / 60), 2) AS total_minutos
+	FROM SpotifyClone.usuario AS usu
+    INNER JOIN SpotifyClone.historico_de_reproducoes AS hist
+		ON usu.usuario_id = hist.usuario_id
+	INNER JOIN SpotifyClone.cancoes AS can
+		ON hist.cancoes_id = can.cancoes_id
+	GROUP BY hist.usuario_id
+    ORDER BY usu.nome ASC;
+    
