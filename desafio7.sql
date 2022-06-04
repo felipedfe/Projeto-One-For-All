5e6bc1a928bac2f518adf70c44dd7334a308545d
Felipe Fernandes <felipedfe@gmail.com>
2022-06-04 07:27:31 -0300

    requisito 7

---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+SELECT
+art.nome AS artista,
+alb.nome AS album, 
+COUNT(seg.usuario_id) AS seguidores
+	FROM SpotifyClone.album AS alb
+    LEFT JOIN SpotifyClone.seguindo_artistas AS seg
+		ON alb.artista_id = seg.artista_id
+	LEFT JOIN SpotifyClone.artista AS art
+		ON alb.artista_id = art.artista_id
+	GROUP BY alb.nome, art.artista_id
+    ORDER BY seguidores DESC, artista ASC, album ASC;
